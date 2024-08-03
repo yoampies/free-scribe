b0f7e7d144ce2e9e73f3fe2637c740f36c2195b1
yoampies <andresampies@gmail.com>
2024-08-03 16:39:02 -0400

    started header and gomepage components

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 justify-center'>
+        <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>
+            Free<span className='text-blue-400 bold'>Scribe</span>
+        </h1>
+    </main>
+  )
+}
